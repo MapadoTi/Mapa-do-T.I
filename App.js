e9929012e1ca923e6b4519362fe34f7b43e8bb44
nathvictoria <victorianath06@gmail.com>
2024-04-23 11:27:46 -0300

    organização do código

--- App.js
+++ App.js
@@ -3,30 +3,16 @@ import { View, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import BemVindo from './BemVindo';
-
-function BemVindo() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-     <Text>SEJA BEM VIND@ AO MAPA DO T.I!</Text>
-      <Button
-        title="LOGIN"
-        onPress={() => navigation.navigate('login')}
-      />
-    </View>
-  );
-}
+import {BemVindo} from './BemVindo';
 
 const Stack = createNativeStackNavigator();
 
-function App() {
-  return (
+export default function App() {
+  return(
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="BemVindo" component={BemVindo} />
+      <Stack.Navigator initialRouteName="BemVindo">
+        <Stack.Screen name="BemVindo" component={BemVindo}/>
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
-
-export default App;
--- BemVindo.js
+++ BemVindo.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { TouchableOpacity, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function BemVindo({ navigation }) {
+export default function BemVindo({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-     
+      <Text>SEJA BEM VIND@ AO MAPA DO T.I!</Text>
+      <View>
+        <TouchableOpacity>
+      title="FAÇA SEU LOGIN"
+    onPress={() => navigation.navigate('login')}
+    </TouchableOpacity>
+      </View>
     </View>
   );
 }
