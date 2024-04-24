862b6ac76daa1252eb4ebf91509da491b9a0c402
Isaque Lima <100887864+Isxqwe@users.noreply.github.com>
2024-04-24 10:01:11 -0300

    navegação 3 telas, menu, login e home e inputs

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import * as React from 'react';
-import { View, Text, Button} from 'react-native';
+import { View, Text, Button, SafeAreaView, StyleSheet} from 'react-native';
+import { TextInput } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const App = () => {
+  const [text, setText] = React.useState("");
 
 function BemVindo({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>SEJA BEM VIND@ AO MAPA DO T.I!</Text>
+      <Text>SEJA BEM VINDO AO MAPA DO T.I!</Text>
       <View>
         <Button
       title="FAÇA SEU LOGIN"
+    onPress={() => navigation.navigate('Login')}
+      />
+      </View>
+    </View>
+  );
+}
+
+function Login({ navigation }) {
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text> Preencha o campo:</Text>
+      <TextInput
+        label="Email"
+        value={text}
+        onChangeText={text => setText(text)}
+      /><TextInput
+        label="Senha"
+        value={text}
+        onChangeText={text => setText(text)}
+      />
+      <View>
+        <Button
+      title=">"
     onPress={() => navigation.navigate('Home')}
       />
       </View>
@@ -22,26 +48,20 @@ function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'top', justifyContent: 'left' }}>
       <Text>Ola Usuario</Text>
-      <Button
-        title="Ola Usuario"
-        onPress={() => navigation.navigate('Details')}
-      />
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return(
+  return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="BemVindo" component={BemVindo} />
+        <Stack.Screen name="Olá Usuário" component={BemVindo} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
-
 
 export default App;
