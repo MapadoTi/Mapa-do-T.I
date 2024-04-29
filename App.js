1d18a2ea61c71b35df1b1cd182c686f0401894a0
Isaque Lima <100887864+Isxqwe@users.noreply.github.com>
2024-04-29 08:23:40 -0300

    junção back + front com primeiro css

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Button, SafeAreaView, StyleSheet} from 'react-native';
+import { View, Text, Button, SafeAreaView, StyleSheet, TouchableOpacity} from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -12,10 +12,20 @@ function BemVindo({ navigation }) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>SEJA BEM VINDO AO MAPA DO T.I!</Text>
       <View>
-        <Button
-      title="FAÇA SEU LOGIN"
-    onPress={() => navigation.navigate('Login')}
-      />
+      <TouchableOpacity style={{
+          margin: 12,
+          height: 50,
+          width: 120,
+          borderRadius: 30,
+          backgroundColor: 'blue', // Changed from 'background' to 'backgroundColor'
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderWidth: 0, // Changed from 'border' to 'borderWidth'
+          fontSize: 16,
+        }}
+          onPress={() => navigation.navigate('Login')}>
+          <Text style={{ color: 'white' }}>Proximo</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -35,10 +45,20 @@ function Login({ navigation }) {
         onChangeText={text => setText(text)}
       />
       <View>
-        <Button
-      title=">"
-    onPress={() => navigation.navigate('Home')}
-      />
+      <TouchableOpacity style={{
+          margin: 12,
+          height: 50,
+          width: 120,
+          borderRadius: 30,
+          backgroundColor: 'blue', // Changed from 'background' to 'backgroundColor'
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderWidth: 0, // Changed from 'border' to 'borderWidth'
+          fontSize: 16,
+        }}
+          onPress={() => navigation.navigate('Home')}>
+          <Text style={{ color: 'white' }}>Faça login</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -46,7 +66,7 @@ function Login({ navigation }) {
 
 function Home({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'top', justifyContent: 'left' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Ola Usuario</Text>
     </View>
   );
