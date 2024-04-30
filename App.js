89fc3a8df53ed31679beaaf8272ec778f4b0533c
Isaque Lima <100887864+Isxqwe@users.noreply.github.com>
2024-04-30 10:42:06 -0300

    background com ajuste automático de tela

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ImageBackground,StyleSheet,Dimensions } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { TextInput } from 'react-native-paper';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const imgbkg1='./assets/welcome.png'
+///1080 por 1920
 function BemVindo({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>SEJA BEM VINDO AO MAPA DO T.I!</Text>
-      <TouchableOpacity
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <ImageBackground source={require('./assets/welcome.png')} style={styles.image}>
+    <View>
+    <TouchableOpacity
         style={{
           margin: 12,
           height: 50,
@@ -18,16 +23,20 @@ function BemVindo({ navigation }) {
           backgroundColor: 'blue',
           justifyContent: 'center',
           alignItems: 'center',
+          top: 100,
           borderWidth: 0,
           fontSize: 16,
         }}
         onPress={() => navigation.navigate('Login')}
       >
-        <Text style={{ color: 'white' }}>Próximo</Text>
+      <Text style={{ color: 'white' }}>Próximo</Text>
       </TouchableOpacity>
+      </View>
+      </ImageBackground>
     </View>
   );
 }
+
 
 function Login({ navigation }) {
   const [email, setEmail] = React.useState('');
@@ -83,7 +92,9 @@ function Home() {
 function Descarte() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>IMAGEM</Text>
+      <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
+       style={{width: 400, height: 700}} />
+
     </View>
   );
 }
@@ -101,11 +112,33 @@ const Stack = createNativeStackNavigator();
 function StackNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="BemVindo" component={BemVindo} />
+      <Stack.Screen name="BemVindo" component={BemVindo} options={{ headerShown: false }} />
       <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: windowWidth, // Define a largura da imagem como a largura da tela
+    height: windowHeight, // Define a altura da imagem como a altura da tela
+    resizeMode: 'contain', // Redimensiona a imagem para caber completamente
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000a0',
+  },
+});
 
 const Drawer = createDrawerNavigator();
 
@@ -114,8 +147,7 @@ function DrawerNavigator() {
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen name="Home" component={Home} />
-        
-<Drawer.Screen name="Descarte" component={Descarte}/>
+        <Drawer.Screen name="Descarte" component={Descarte}/>
         <Drawer.Screen name="Doação" component={Doação}/>
         <Drawer.Screen name="Logout" component={StackNavigator} options={{ headerShown: false }} />
 
